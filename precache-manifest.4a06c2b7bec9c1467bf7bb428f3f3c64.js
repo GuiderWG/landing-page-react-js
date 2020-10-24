9f29a97e22879520a3dcc1500a82e68368a41c03
Vladimir <guiderwg@gmail.com>
2020-10-24 18:23:18 +0400

    Updates

---
+++ precache-manifest.4a06c2b7bec9c1467bf7bb428f3f3c64.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f91e635874c7847be4c7f53039ea5b4",
+    "url": "/landing-page-react-js/index.html"
+  },
+  {
+    "revision": "855a5b6bb684afc38465",
+    "url": "/landing-page-react-js/static/css/main.7d152527.chunk.css"
+  },
+  {
+    "revision": "10aa5573105790873488",
+    "url": "/landing-page-react-js/static/js/2.6bf6e53b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/landing-page-react-js/static/js/2.6bf6e53b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "855a5b6bb684afc38465",
+    "url": "/landing-page-react-js/static/js/main.e3be9d7a.chunk.js"
+  },
+  {
+    "revision": "fe79e80903bec372df82",
+    "url": "/landing-page-react-js/static/js/runtime-main.6f11e261.js"
+  },
+  {
+    "revision": "6b764ed82fd5c663a6c7223246368d9d",
+    "url": "/landing-page-react-js/static/media/video.6b764ed8.mp4"
+  }
+]);
